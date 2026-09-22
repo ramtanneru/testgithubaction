e8786611e8225aa9ef7ef80cc298fc479d974858
rtanneru <rtanneru@U-2N71NN34M61UP.wspoc.qts>
2026-09-22 13:35:43 -0400

    Unit Test Cases updated

---
+++ tests/test_operations.py
@@ -0,0 +1,19 @@
+from src.math_operations import add, sub
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+def test_sub():
+    assert sub(5, 3) == 2
+    assert sub(0, 5) == -5
+
+def test_mul():
+    assert mul(2, 3) == 6
+    assert mul(-2, 3) == -6
+
+def test_div():
+    assert div(6, 3) == 2
+    assert div(0, 5) == 0
+
+    
